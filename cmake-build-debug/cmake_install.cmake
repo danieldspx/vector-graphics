b4e62901dc8ab4daa2873b785f5f094c98f497de
Daniel Pereira <danield1591998@gmail.com>
2023-04-12 17:25:50 -0300

    chore: add clion files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/daniel/Documentos/UFSM/CG/canvas-t1
+# Install script for directory: /home/daniel/Documentos/UFSM/CG/t1-vector-graphics
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/daniel/Documentos/UFSM/CG/canvas-t1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/daniel/Documentos/UFSM/CG/t1-vector-graphics/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
